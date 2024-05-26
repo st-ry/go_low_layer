c9db64c2b762f0f32f7ea4c097531edf5c3fdab2
st-ry <stry.makorepe@gmail.com>
2024-05-26 21:44:25 +0900

    update: samples using interface to check gzip filter and bufio filter

--- 02/main.go
+++ 02/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
+	"compress/gzip"
 	"fmt"
 	"io"
 	"net"
+	"net/http"
 	"os"
 )
 
@@ -25,12 +28,36 @@ func main(){
 	buffer.Write([]byte("bytes.Buffer example\n"))
 	fmt.Println(buffer.String())
 	
+	//bufio
+	bufferIo := bufio.NewWriter(os.Stdout)
+	bufferIo.WriteString("bufio.Writer ")
+	bufferIo.Flush()
+	bufferIo.WriteString("example\n")
+	bufferIo.Flush()
+	
 	//net
 	conn, err := net.Dial("tcp", "ascii.jp:80")
 	if err != nil {
 		panic(err)
 	}
-	
 	conn.Write([]byte("GET / HTTP/1.0\r˜Host:ascii.jp\r\n\r\n"))
 	io.Copy(os.Stdout, conn)
+	
+	//gzip
+	gzfile, err := os.Create("test.txt.gz")
+	if err != nil {
+		panic(err)
+	}
+	writer := gzip.NewWriter(gzfile)
+	writer.Header.Name = "text2.txt"
+	writer.Write([]byte("gzip.Writer example\n"))
+	writer.Close()
+	
+	//http server
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("http.ResponseWriter Sample"))
 }
