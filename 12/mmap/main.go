f7a5470263eb22af979fbc171283e1554bad6f78
st-ry <stry.makorepe@gmail.com>
2024-06-27 22:15:27 +0900

    update: mapping data to memory sample

--- 12/mmap/main.go
+++ 12/mmap/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"os"
 	"path/filepath"
+
+	"github.com/edsrzf/mmap-go"
 )
 
 func main() {
@@ -18,4 +22,23 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	defer f.Close()
+	m, err := mmap.Map(f, mmap.RDWR, 0)
+	if err != nil {
+		panic(err)
+	}
+	defer m.Unmap()
+	
+	//update data on memory and write
+	m[9] = 'X'
+	m.Flush()
+	
+	//read
+	fileData, err := io.ReadAll(f)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("original: %s\n", testData)
+	fmt.Printf("mmap: %s\n", m)
+	fmt.Printf("file: %s\n", fileData)
 }
